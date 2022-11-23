77ba7a2b8af025e812c7a272f566fe4ff10807d6
aashay shah <aashayshah28@gmail.com>
2022-11-23 16:50:43 +0530

    Change Bio

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Aashay Shah",
   title: "Software Engineer",
   desciption:
-    "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+    "Front End Developer My role is to write and style the front-end components that meet the requirements of our mocks and fulfill our client stories.",
   image:
     "https://media-exp1.licdn.com/dms/image/D4D03AQFXPiO8WyUl4Q/profile-displayphoto-shrink_800_800/0/1666951722635?e=2147483647&v=beta&t=yU73uWSX1GvROWh7BRLfwhePNlMwfKp2MjYx1os58tY",
   resumePdf: resume,
